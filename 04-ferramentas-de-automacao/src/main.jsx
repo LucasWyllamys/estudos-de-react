40a730de0f1b16b8a0e2c53209ddc9b823363c3e
LUCAS WYLLAMYS CARMO DA SILVA <127877422+LucasWyllamys@users.noreply.github.com>
2024-10-30 18:39:54 -0300

    30.10.2024-18:39

---
+++ 04-ferramentas-de-automacao/src/main.jsx
@@ -0,0 +1,9 @@
+// Importação das bibliotecas React:
+import ReactDOM from 'react-dom/client';
+import React from 'react';
+//---------------------------
+import App from './App';
+
+// ReactDOM.createRoot(document.getElementById(root)) criação da raiz.
+// .render() renderiza a aplicação.
+ReactDOM.createRoot(document.getElementById('root')).render(<App />);
